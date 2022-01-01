7af9faa8ea1b4d4acd0432defb73486076d1a3ab
Hrafnskogr <hrafnskogr@gmail.com>
2022-01-01 12:00:31 +0100

    Doc, Metadata and License update

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,105 @@
+//! # bitf
+//! Rust procedural macro to quickly generate bitfield from a structure.
+//!
+//! ## Usage
+//! The macro can be used as following:
+//! ```rust
+//! #[bitf(size, order)]
+//!
+//! // Where size can be:
+//! u8
+//! u16
+//! u32
+//! u64
+//! u128
+//!
+//! // And order can be lsb or msb
+//! ```
+//! The size parameter will constrain the total size of the bitfield.
+//! The order parameter will alter the order in which the fields are declared.
+//! When setting the order parameter to msb, the first declared field of the struct will be set on the most significant bit, and the other way around when using the lsb mode.
+//!
+//! The size and position of the field is based on the field declaration :
+//! ``` rust
+//! struct Example
+//! {
+//!   any_case_name_intBitfieldSize: BogusType,
+//! }
+//! ```
+//!
+//! Finally, the internal, full value of the field can be accessed as :
+//! ``` rust
+//! let e = Example::default();
+//! println!("{}", e.raw);
+//!
+//! ```
+//! 
+//!
+//! ## Example
+//!
+//! Considering the following bitfield:
+//!
+//! ```
+//! 7             0
+//! 0 0 0 0 0 0 0 0
+//! | | | | | | | |_ field1    - Size 1
+//! | | | | | | |___ field2    - Size 1
+//! | | | | | |_____ field3    - Size 1
+//! | |  \|/________ reserved  - Size 3
+//! \ /_____________ field4    - Size 2
+//!
+//! ```
+//! It can be achieved with the following declaration and macro usage
+//!
+//! ```rust
+//! #[bitf(u8, lsb]
+//! struct MyStruct
+//! {
+//!   field_a_1:   (),
+//!   fieldA_1:   (),
+//!   FieldA_1:   (),
+//!   reserved_3: (),
+//!   Field_A_2:   (),
+//! }
+//! ```
+//!
+//! This will generate the following structure and associated methods
+//!
+//! ```rust
+//! struct MyStruct
+//! {
+//!   pub raw:  u8,
+//! }
+//!
+//! impl MyStruct
+//! {
+//!   fn field_a() -> u8 { ... }
+//!   fn set_field_a(val: u8) { ... }
+//! }
+//!
+//! impl Default for MyStruct { ... }
+//! ```
+//!
+//! So you can easily set and read values of each defined bitfield:
+//!
+//! ```rust
+//! let mut bf = MyStruct::default;
+//! bf.set_field1(1);
+//! println!("{:#010b}", bf.field1());
+//! ```
+
+
+
+
+
+
+
+
+
+
+
+
+
 /*
  * Main source file for the 'bitf' procedural macro definition
  * usage: #[bitf(size_of_bitfield, ordering_of_field)]
@@ -29,10 +131,10 @@ pub fn bitf(_meta: TokenStream, _input: TokenStream) -> TokenStream
     let name = strukt.name.clone();
     // Extract fields for quote! code generation
     let mut bfields = strukt.bfields.clone();
-  
+
     // Get the parameters passed in the attribute
     let params = syn::parse_macro_input!(_meta as MacroParams);
-    // Extract type to be returned by the redefined structure, for use in quote! code generation 
+    // Extract type to be returned by the redefined structure, for use in quote! code generation
     let ret_type = params.ty;
     // Extract the size of the bitfield, for use in quote! code generation
     let bfield_size = params.bitfield_size;
@@ -71,7 +173,7 @@ pub fn bitf(_meta: TokenStream, _input: TokenStream) -> TokenStream
                                         let mask = #ret_type::MAX >> (#bfield_size - #fsize) << #fpos;
                                         (self.raw & mask) >> #fpos
                                     }
-                                    
+
                                     #[inline]
                                     #[allow(non_snake_case)]
                                     pub fn #set_n(self: &mut Self, val: #ret_type)
@@ -115,5 +217,3 @@ pub fn bitf(_meta: TokenStream, _input: TokenStream) -> TokenStream
             }
         )
 }
-
-
